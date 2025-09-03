ba7bbca3ae8843597e5d71b491f26f474ae0674b
Davi Belo <davibelocontato@gmail.com>
2025-09-03 20:03:14 +0100

    feat/solutions: full solution section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@/components/templates/Header/Header'
 import { Hero } from '@/components/templates/Hero/Hero'
 import { Mission } from '@/components/templates/Mission/Mission'
+import { Solutions } from '@/components/templates/Solutions/Solutions'
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
       <Mission />
+      <Solutions />
     </>
   )
 }
